3d2dada10cc8081081ea840c02af4cddb0faed91
joseflauzino <jose.wilson.vf@gmail.com>
2020-09-02 17:04:04 -0300

    Fixing data access

--- access_interface/vines_leaf_driver.py
+++ access_interface/vines_leaf_driver.py
@@ -15,14 +15,14 @@ def vnf_status(args):
 	response = run_vnf_request_cmd(args, cmd)
 	if response["status"] == "ERROR":
 		return {'status':'error','data':"could not get vnf status"}
-	return {'status':'success','data':response}
+	return {'status':'success','data':response["data"]}
 
 def nf_status(args):
 	cmd = _build_cmd("GET", _create_url(find_by_key(args,"vnf_ip"), "running"))
 	response = run_vnf_request_cmd(args, cmd)
 	if response["status"] == "ERROR":
 		return {'status':'error','data':"could not get network function status"}
-	return {'status':'success','data':response}
+	return {'status':'success','data':response["data"]}
 
 def push_vnfp(args):
 	router_ip = find_by_key(args,"router_ip")
@@ -40,35 +40,35 @@ def push_vnfp(args):
 	response = run_shell_cmd(ssh_cmd)
 	if response["status"] == "ERROR":
 		return {'status':'error','data':"could not push vnfp (ssh error)"}
-	return {'status':'success','data':response}
+	return {'status':'success','data':response["data"]}
 
 def install(args):
 	cmd = _build_cmd("POST", _create_url(find_by_key(args,"vnf_ip"), "install"))
 	response = run_vnf_request_cmd(args, cmd)
 	if response["status"] == "ERROR":
 		return {'status':'error','data':"could not install function"}
-	return {'status':'success','data':response}
+	return {'status':'success','data':response["data"]}
 
 def start(args):
 	cmd = _build_cmd("POST", _create_url(find_by_key(args,"vnf_ip"), "start"))
 	response = run_vnf_request_cmd(args, cmd)
 	if response["status"] == "ERROR":
 		return {'status':'error','data':"could not start function"}
-	return {'status':'success','data':response}
+	return {'status':'success','data':response["data"]}
 
 def stop(args):
 	cmd = _build_cmd("POST", _create_url(find_by_key(args,"vnf_ip"), "stop"))
 	response = run_vnf_request_cmd(args, cmd)
 	if response["status"] == "ERROR":
 		return {'status':'error','data':"could not stop function"}
-	return {'status':'success','data':response}
+	return {'status':'success','data':response["data"]}
 
 def get_log(args):
 	cmd = _build_cmd("GET", _create_url(find_by_key(args,"vnf_ip"), "log"))
 	response = run_vnf_request_cmd(args, cmd)
 	if response["status"] == "ERROR":
 		return {'status':'error','data':"could not get function log"}
-	return {'status':'success','data':response}
+	return {'status':'success','data':response["data"]}
 
 
 #------------------------------------------------------------------
