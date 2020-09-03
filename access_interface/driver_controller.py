795589c649187168a09d9b8d2bac8b0ee648cd66
José Flauzino <jose.wilson.vf@gmail.com>
2020-09-03 20:03:55 -0300

    Handling driver not found exeption

--- access_interface/driver_controller.py
+++ access_interface/driver_controller.py
@@ -45,6 +45,10 @@ class DriverController():
 	def handle_call(self, method_name, args):
 		# instantiate the correct driver and method
 		method = getattr(self._search_driver(self.drivers, self._find_by_key(args,'vnf_platform')), method_name)
+		# check if driver was found
+		if str(type(method))=="NoneType":
+			print "VNF platform driver %s not found"
+			return {"status":"ERROR","data":"VNF Platform Driver not found"}
 		# call the method
 		return method(args)
 		# return format {'status':'success or error','data':'the content data'}
