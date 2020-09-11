cb4b1000a77630d43bd2b3ba8477271e3d253e94
root <jose.wilson.vf@gmail.com>
2020-09-11 18:12:21 -0300

    Adding prints

--- access_interface/util.py
+++ access_interface/util.py
@@ -11,6 +11,8 @@ ssh_port = "3922"
 def run_shell_cmd(cmd):
 	process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 	output, error = process.communicate()
+	print "Output:%s" % output
+	print "error:%s" % error
 	if process.returncode != 0:
 		print "Error on command"
 		return {"status":"ERROR","data":output}
