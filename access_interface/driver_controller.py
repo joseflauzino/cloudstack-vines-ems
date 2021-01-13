862021a08a14c9fd60717291f060c6038032213a
joseflauzino <jose.wilson.vf@gmail.com>
2021-01-12 23:17:06 -0300

    Fixing database module

--- access_interface/driver_controller.py
+++ access_interface/driver_controller.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
-import os
-from base_manager import *
+import os,sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir) 
+from vib_client import *
 
 #==================================================================
 #                 Vines - Element Management System
