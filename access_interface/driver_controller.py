735a911f455f1b3db2a863e16067a06e5c0972ac
joseflauzino <jose.wilson.vf@gmail.com>
2021-01-15 17:07:19 -0300

    Fixing imports

--- access_interface/driver_controller.py
+++ access_interface/driver_controller.py
@@ -12,32 +12,31 @@ import sys
 import imp
 from vib import vib_client
 
+##################################################################
+####################### DriverController #########################
+##################################################################
+
 class DriverController():
+
+	#------------------------------------------------------------------
 	# Private methods
+	#------------------------------------------------------------------
 	def __init__(self):
-		self.drivers_path = os.path.dirname(os.path.realpath(__file__))+"/drivers"
+		self.drivers_path = os.path.dirname(os.path.realpath(__file__))+"/extended_drivers"
 		self.drivers = self._import_drivers(self._read_drivers()) # load all drivers
 
 	def _read_drivers(self):
-		# TODO: try to allocate all drivers in a specific folder, so that it is not
-		# necessary to insert '_driver.py' in the names of the driver files
 		drivers_names = []
 		for currentFolder, subFolder, files in os.walk(self.drivers_path):
-			print("currentFolder: "+str(currentFolder))
-			print("files: "+str(files))
 			for file in files:
 				if file.endswith("-driver.py"):
 					drivers_names.append(file.replace(".py",""))
-			#drivers_names = [os.path.join(file.replace(".py","")) for file in files if file.endswith("-driver.py")]
-		print("Drivers names: "+str(drivers_names))
 		return drivers_names
 
 	def _import_drivers(self, drivers_names):
 		drivers = []
 		for driver in drivers_names:
-			#drivers.append(__import__(driver))
 			drivers.append(imp.load_source(driver, self.drivers_path+"/"+driver+".py"))
-		print("Imported drivers: "+str(drivers))
 		return drivers
 
 	def _search_driver(self, drivers, driver_type):
@@ -53,17 +52,15 @@ class DriverController():
 					return current_value
 		return None
 
+	#------------------------------------------------------------------
 	# Public methods
+	#------------------------------------------------------------------
 	def handle_call(self, method_name, args):
-		# find VNF in base
 		vnf_id = self._find_by_key(args,'vnf_id')
 		result = vib_client.find_vnf(vnf_id)
 		if result["success"] == False:
 			return {'status':'error','data':result["data"]}
-		# add VNF data into args
 		args.append({"vnf_ip":result["data"][0]["ip"]})
 		args.append({"vnf_platform":result["data"][0]["vnf_exp"]})
-		# instantiate the correct driver and method
 		method = getattr(self._search_driver(self.drivers, self._find_by_key(args,'vnf_platform')), method_name)
-		# call the method
 		return method(args)
