d031e40a58e317bce53174ec039559fffda91892
joseflauzino <jose.wilson.vf@gmail.com>
2021-01-26 15:09:24 -0300

    Test2

--- access_interface/api.py
+++ access_interface/api.py
@@ -50,7 +50,7 @@ def ems_vnfs():
         args.append({"vnf_platform":str(request.json['vnf_platform'])})
 
         if request.json.get('fault_monitoring_policy'): # there is a fault monitoring policy
-            print("Tipo do dado: "+string(type(request.json['fault_monitoring_policy'])))
+            print("Tipo do dado: "+str(type(request.json['fault_monitoring_policy'])))
             args.append({"fault_monitoring_policy":request.json['fault_monitoring_policy']})
 
         response = vib_client.add_vnf(args)
